36cc7fd464b62bd7e1cdfa9f975f6bcc32ec6c08
Kahen <KahenLee@163.com>
2020-02-19 14:03:51 +0800

    首页导航模块

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    // 首页导航数据
+    navList:[],
+    //被点击的首页导航菜单索引
+    currentIndexNav:0
+  },
+
+  /**
+   * 获取首页导航数据
+   */
+  getNavList(){
+    let that=this;
+    // 利用小程序内置发送请求方法
+    wx.request({
+      url:"https://mockapi.eolinker.com/7b7NMB9c75d613bc39c8f16e4e03a3d4a8f951750079dc5/navList",
+      success(res){
+        // console.log(res);
+        if(res.data.code===0){
+          that.setData({
+            navList:res.data.data.navList
+          })
+        }
+      }
+    })
+  },
+  /**
+   * 点击首页导航数据
+   */
+  activeNav(e){
+    this.setData({
+      currentIndexNav:e.target.dataset.index
+    })
   },
 
   /**
@@ -18,7 +48,8 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    // 1 获取首页导航数据
+    this.getNavList();
   },
 
   /**
